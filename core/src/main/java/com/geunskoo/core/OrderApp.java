13cf18609a418cbde65acee127db0d84973c79bd
Kim taegeun <oryukdo3@pusan.ac.kr>
2023-01-10 01:25:43 +0900

    feat : OrderApp 스프링 컨테이너 이용해보기

---
+++ core/src/main/java/com/geunskoo/core/OrderApp.java
@@ -0,0 +1,22 @@
+package com.geunskoo.core;
+
+import com.geunskoo.core.domain.Grade;
+import com.geunskoo.core.domain.Member;
+import com.geunskoo.core.domain.MemberService;
+import com.geunskoo.core.order.Order;
+import com.geunskoo.core.order.OrderService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Order order = orderService.createOrder(1L, "itemA", 20000);
+        System.out.println("order = " + order);
+    }
+}
