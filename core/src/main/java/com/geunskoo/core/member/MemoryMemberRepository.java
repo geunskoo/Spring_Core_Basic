44911e6bcd9900f237db4420f35eeb9ac4a4f559
Kim taegeun <oryukdo3@pusan.ac.kr>
2023-01-10 15:43:14 +0900

    refactor : 애노테이션을 이용한 componentScan 및 Autowired

--- core/src/main/java/com/geunskoo/core/member/MemoryMemberRepository.java
+++ core/src/main/java/com/geunskoo/core/member/MemoryMemberRepository.java
@@ -1,7 +1,9 @@
 package com.geunskoo.core.member;
 
-import java.util.HashMap;
+import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+@Component
 public class MemoryMemberRepository implements MemberRepository {
     private static HashMap<Long, Member> store = new HashMap<>(); //ConcurrentHashMap 사용을 권장.
 
--- core/src/main/java/com/geunskoo/core/order/OrderServiceImpl.java
+++ core/src/main/java/com/geunskoo/core/order/OrderServiceImpl.java
@@ -3,11 +3,15 @@ package com.geunskoo.core.order;
 import com.geunskoo.core.discount.DiscountPolicy;
 import com.geunskoo.core.member.Member;
 import com.geunskoo.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
