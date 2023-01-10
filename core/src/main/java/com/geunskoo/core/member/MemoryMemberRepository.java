9fa4b09093922c8efa0204a6f7c118597ece81c9
Kim taegeun <oryukdo3@pusan.ac.kr>
2023-01-10 16:12:11 +0900

    refactor :  @RequriedArgsConstructor적용

--- core/src/main/java/com/geunskoo/core/member/MemoryMemberRepository.java
+++ core/src/main/java/com/geunskoo/core/member/MemoryMemberRepository.java
@@ -3,6 +3,7 @@ package com.geunskoo.core.member;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+
 @Component
 public class MemoryMemberRepository implements MemberRepository {
     private static HashMap<Long, Member> store = new HashMap<>(); //ConcurrentHashMap 사용을 권장.
